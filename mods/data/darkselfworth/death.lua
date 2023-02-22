aa14d8433319649b8cde2e1b4105f1a486a15e09
Gazozoz <88601721+Gazozoz@users.noreply.github.com>
2023-02-22 08:20:56 -0300

    finally

--- mods/data/darkselfworth/death.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-    setPropertyFromClass('GameOverSubstate', 'characterName', 'scott_ded'); --Character json file for the death animation
-    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'scott_death_sfx'); --put in mods/sounds/
-    setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'well_shit'); --put in mods/music/
-    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'rythis_confirm'); --put in mods/music/
-end
--- mods/data/redemption/invgfnote.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	for i = 0, getProperty('unspawnNotes.length')-1 do
-		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == "GF Sing" then
-			setPropertyFromGroup('unspawnNotes', i, 'visible', false)
-		end
-	end
-end
